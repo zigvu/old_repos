1e4f1c337e23b5abdc5237a2af30df7c72714e0b
eacharya <eacharya@gmail.com>
2016-01-25 11:06:03 -0800

    Master: 25 Jan 2016: Backup anarsa, cellroti, kheer and momo

---
+++ get_repo_info.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Usage:"
+echo "./get_repo_info.sh <repoName>"
+
+
+repo=$1
+filename=$(echo "$repo.json" | tr / -)
+echo "Dumping $1 to $filename..."
+echo
+
+read -s -p "Password for repo: " pass
+
+curl -u "eacharya:$pass" \
+  "https://api.github.com/repos/zigvu/$1/issues?per_page=1000&state=all" \
+  > $filename
+
+git clone "https://eacharya:$pass@github.com/zigvu/$repo.wiki.git"
